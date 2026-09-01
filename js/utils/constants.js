ef12e18b8fa5f58ee6cf20a96282c316136ae534
prasbudi1982 <68359145+prasbudi1982@users.noreply.github.com>
2026-09-02 00:27:24 +0700

    Add files via upload

--- js/utils/constants.js
+++ js/utils/constants.js
@@ -1,1 +1,4 @@
 
+export const PIN_KEY = 'kasir_stock_pin_hash_v1';
+export const DEFAULT_PIN = '1234';
+export const APP_KEYS = ['kasir_app_settings','kasir_last_trx','kasir_network_printer','kasir_perpage','kasir_printer_name','kasir_products','kasir_produk_v2','kasir_promo_bundles','kasir_stock_logs','kasir_toko_settings','kasir_transactions'];
